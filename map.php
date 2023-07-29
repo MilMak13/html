ac35e25574f10b7b2cee235ef5227ca71b47fb8f
MilMak13 <140755190+MilMak13@users.noreply.github.com>
2023-07-29 13:04:44 +0300

    Add files via upload

---
+++ map.php
@@ -0,0 +1,381 @@
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		
+			<!doctype html>
+<html lang="en-US">
+<head>
+<title>Розыск</title>
+<meta charset="utf-8">
+<meta name="description" content="Our Roadmap is a non-exhaustive list of the current goals we aim to achieve through new developments and continuous integration, made possible with the support of our generous patrons.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, roadmap">
+<meta name="author" content="Delftia">
+<meta name="copyright" content="Delftia">
+<link rel="icon" type="image/png" href="/img/meta/favicon/16.png" sizes="16x16"/>
+<link rel="icon" type="image/png" href="/img/meta/favicon/32.png" sizes="32x32"/>
+<link rel="icon" type="image/png" href="/img/meta/favicon/96.png" sizes="96x96"/>
+<link rel="icon" type="image/png" href="/img/meta/favicon/128.png" sizes="128x128"/>
+<link rel="icon" type="image/png" href="/img/meta/favicon/196.png" sizes="196x196"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/57.png" sizes="57x57"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/60.png" sizes="60x60"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/72.png" sizes="72x72"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/76.png" sizes="76x76"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/114.png" sizes="114x114"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/120.png" sizes="120x120"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/144.png" sizes="144x144"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/152.png" sizes="152x152"/>
+<link rel="manifest" href="/lib/js/manifest.json">
+<meta name="theme-color" content="#686de0">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><meta property="og:title" content="Delftia - Roadmap" />
+<meta property="og:description" content="Our Roadmap is a non-exhaustive list of the current goals we aim to achieve through new developments and continuous integration, made possible with the support of our generous patrons." />
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://rpcs3.net" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="Delftia" />
+
+<meta name="twitter:title" content="Delftia - Roadmap">
+<meta name="twitter:description" content="Our Roadmap is a non-exhaustive list of the current goals we aim to achieve through new developments and continuous integration, made possible with the support of our generous patrons.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<link rel="stylesheet" type="text/css" href="/lib/css/anim.css?v1"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/dark.css?v7"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/main.css?v1"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/debug.css?v1"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/scale.css?v1"/>
+<script data-cfasync="false" src='/lib/js/jquery.js?v1'></script>
+<script data-cfasync="false" src='/lib/js/dark.js?v5'></script>
+<script data-cfasync="false" src='/lib/js/main.js?v1'></script>
+<script data-cfasync="false" src='/lib/js/jscookie.js?v1'></script>
+<script data-cfasync="false" src='/lib/js/particles.js?v1'></script>
+<script data-cfasync="false" src='/lib/js/particles-sel.js?v1'></script>
+<noscript>
+<style>
+/*
+General:
+- Elements that fade in are not visible by default
+*/
+.fade-up-onstart {
+	display:inherit;
+}
+.fade-in-onload {
+	display:inherit;
+}
+/*
+Index:
+- Center index contents
+*/
+.landing-con-container {
+	top:50%;
+	margin-top:-139px;
+}
+/*
+Quickstart:
+- Remove dimming
+- Do not block interaction outside the sidebar
+- Slightly reduce the width of the sidebar
+*/
+.toggle-navsidebar {
+	background:none;
+	width:auto;
+}
+.sidebar-con-anim {
+	width:300px;
+}
+</style>
+</noscript>
+</head>
+<body>
+<div class='menu-con-release'>
+	<span>Website Release Preview 2023.06.20</span>
+</div>
+<div class='mini-menu-con-dimmer'>
+	<div class='mini-menu-con-container'>
+		
+		<div class='mini-menu-tx1-block'>
+			<div class='mini-menu-emp-block'>
+			</div>
+			<h2>No Piracy.</h2>
+		</div>
+		<div class='mini-menu-tx2-block'>
+			<p>
+				 <u>Сайт является игровым по игре Minecraft и не может быть связан с реальной жизнью.</u>
+			</p>
+		</div>
+		<div class="mini-menu-btn-agree">
+			<h2>Я принимаю.</h2>
+		</div>
+	</div>
+</div>
+<div class='menu-con-message fade-out-onload'>
+	<div class='menu-tx1-message toggle-message fade-out-onload'>
+		<span>В настоящее время мы сталкиваемся с более высоким объемом сервера, чем обычно. Мы приносим искренние извинения за доставленные неудобства.</span>
+	</div>
+</div>
+<div class="window-con-theme">
+	<div class="window-bg-theme">
+	</div>
+	<div class="theme-con-spinner">
+		<div class="theme-ico-spinner">
+		</div>
+	</div>
+	<div class="theme-tx1-description pulsate">
+		<span>В настоящее время эта функция еще недоступна</span>
+	</div>
+	<div class="wavebar-con-container-theme">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="theme-btn-close toggle-theme">
+</div>
+<div class="settings-menu-con-outer settings-submenu">
+	<div class="settings-menu-con-wrapper toggle-darkmode settings-menu-upper">
+		<div class="settings-menu-ico-darkmode">
+		</div>
+		<div class="settings-menu-btn-button btn-darkmode">
+			<span>Темная тема - On/Off</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-transparency">
+		<div class="settings-menu-ico-transparency">
+		</div>
+		<div class="settings-menu-btn-button btn-transparency">
+			<span>Прозрачность - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-particles">
+		<div class="settings-menu-ico-particles">
+		</div>
+		<div class="settings-menu-btn-button btn-particles">
+			<span>Частицы - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-pulsate">
+		<div class="settings-menu-ico-pulsate">
+		</div>
+		<div class="settings-menu-btn-button btn-pulsate">
+			<span>Пульсирование - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-waves">
+		<div class="settings-menu-ico-waves">
+		</div>
+		<div class="settings-menu-btn-button btn-waves">
+			<span>Волны - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-theme settings-menu-lower" style="border-top: solid 1px rgba(0,0,0,.02);">
+		<div class="settings-menu-ico-theme">
+		</div>
+		<div class="settings-menu-btn-button btn-theme">
+			<span>Изменить тему</span>
+		</div>
+	</div>
+</div>
+<div class="menu-btn-backtotop">
+</div>
+<div class='menu-con-container'>
+	<div class="menu-con-backdrop darkmode-menu">
+	</div>
+	<div class="menu-btn-settings toggle-settings">
+	</div>
+	<div class="menu-con-outer">
+		<div class="menu-con-inner">
+			
+			<div class="mobile-menu-btn-open toggle-mobilemenu">
+			</div>
+			<div class="mobile-menu-btn-darkmode toggle-darkmode">
+			</div>
+			<a href='/index'>
+			<div class="menu-btn-select">
+				<span>Главная</span>
+			</div>
+			</a>
+			<a href='/capital'>
+			<div class="menu-btn-select">
+				<span>О стране</span>
+			</div>
+			</a>
+			<a href='/faq'>
+			<div class="menu-btn-select">
+				<span>Новости</span>
+			</div>
+			</a>
+			<a href='/about'>
+			<div class="menu-btn-select">
+				<span>Законодательство</span>
+			</div>
+			</a>
+			<a href='/compatibility'>
+			<div class="menu-btn-select">
+				<span>Конкурсы и праздники</span>
+			</div>
+			</a>
+			<a href='/download'>
+			<div class="menu-btn-select">
+				<span>Структуры</span>
+			</div>
+			</a>
+			<a href='/quickstart'>
+			<div class="menu-btn-select">
+				<span>Форум</span>
+			</div>
+			</a>
+			<a href='/roadmap'>
+			<div class="menu-btn-select">
+				<span>Розыск</span>
+			</div>
+			</a>
+		
+			<div class="menu-con-divider">
+			</div>
+		
+			</a>
+			
+			</a>
+			<a href='/wiki'>
+			<div class="menu-btn-select">
+				<span>Wiki</span>
+			</div>
+			</a>
+			<a href='https://discord.gg/XXHVUjJ9wp' target="_blank">
+			<div class="menu-btn-select">
+				<span>Discord</span>
+			</div>
+			</a>
+			 <a href='https://www.youtube.com/@rttnrtrnnrttrnr' target="_blank">
+			
+			<div class="menu-btn-select">
+				<span>YouTube</span>
+			</div>
+			</a>
+			
+		</div>
+	</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+	<div class="menu-con-backdrop-2">
+	</div>
+	<div class='mobile-menu-con-scroll'>
+		<div class="mobile-menu-con-outter">
+			<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+				<div class='mobile-menu-tx1-group'>
+					<span>Main</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="/">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Home</span>
+				</div>
+				</a>
+		<a href="/capital">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>О стране</span>
+				</div>
+				</a>
+				<a href="/about">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Законодательство</span>
+				</div>
+				</a>
+				<a href="/compatibility">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Конкурсы</span>
+				</div>
+				</a>
+				<a href="/download">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Структуры</span>
+				</div>
+				</a>
+				<a href="/quickstart">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Форум</span>
+				</div>
+				</a>
+				<a href="/roadmap">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Розыск</span>
+				</div>
+				</a>
+				<a href="/faq">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Новости</span>
+				</div>
+				</a>
+		
+				<a href="/wiki">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Wiki</span>
+				</div>
+				</a>
+				
+				
+				
+				<a href="https://discord.gg/xK474wXayg" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Discord</span>
+				</div>
+				</a>
+				<a href="https://www.youtube.com" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>YouTube</span>
+				</div>
+				</a>
+				
+			</div>
+		</div>
+	</div>
+
+		<div class="wavebar-con-container">
+		</div>
+			
+				
+
+			
+</body>
+
+</html>
